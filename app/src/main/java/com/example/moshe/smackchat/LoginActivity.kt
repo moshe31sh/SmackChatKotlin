18a70fe81187eb2c65517876036d2ccb52188dbd
Moshe Shimon <moshe31sh@gmail.com>
2017-12-03 15:10:33 +0200

    create login activity

---
+++ app/src/main/java/com/example/moshe/smackchat/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.moshe.smackchat
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : BaseActivity() {
+
+
+
+    override fun getContentView(): Int {
+        return R.layout.activity_login
+    }
+
+    override fun init() {
+        loginLoginBtn.setOnClickListener{
+
+        }
+
+        loginCreateUserBtn.setOnClickListener {
+
+        }
+    }
+}
--- app/src/main/java/com/example/moshe/smackchat/MainActivity.kt
+++ app/src/main/java/com/example/moshe/smackchat/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.moshe.smackchat
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -17,17 +14,23 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
     }
 
     override fun getContentView(): Int = R.layout.activity_main
 
     override fun init() {
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        loginBtnNavHeader.setOnClickListener {
+        startActivity(Intent(this,LoginActivity::class.java))
+        }
+
+        addChannelBtn.setOnClickListener {  }
     }
 
     override fun onBackPressed() {
