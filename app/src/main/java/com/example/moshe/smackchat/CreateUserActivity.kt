3b09fb252eb31c9396be462a7c56c459e3de7f63
Moshe Shimon <moshe31sh@gmail.com>
2017-12-03 16:26:53 +0200

    set image and background color

--- app/src/main/java/com/example/moshe/smackchat/CreateUserActivity.kt
+++ app/src/main/java/com/example/moshe/smackchat/CreateUserActivity.kt
@@ -1,19 +1,45 @@
 package com.example.moshe.smackchat
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.graphics.Color
 import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : BaseActivity() {
+
+    var userAvatar = "profileDefault"
+    var userVolor = "[0.5, 0.5, 0.5 , 1]"
+
     override fun getContentView(): Int = R.layout.activity_create_user
 
     override fun init() {
         backgroundColorBtn.setOnClickListener {
+            val ran = Random()
+            val r = ran.nextInt(225)
+            val g = ran.nextInt(225)
+            val b = ran.nextInt(225)
 
+            createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+            val savedR =    r.toDouble() / 255
+            val savedG =    r.toDouble() / 255
+            val savedB =    r.toDouble() / 255
+
+            userVolor = "[$savedR, $savedG, $savedG, 1]"
         }
 
         createAvatarImageView.setOnClickListener {
+            val ran = Random()
+            val color = ran.nextInt(2)
+            val avatar = ran.nextInt(28)
 
+            userAvatar = if(color == 0) {
+                "light$avatar"
+            }else{
+                "dark$avatar"
+            }
+
+            val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+            createAvatarImageView.setImageResource(resourceId)
         }
 
         createUserBtn.setOnClickListener {  }
